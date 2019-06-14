c0301d225a24c4888de355208ada493de834e67a
Stuart Marquis <stuartmarquis@gmail.com>
2019-06-14 12:10:51 -0400

    secrets.yml, for secret_key_base

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/database.yml", "config/app_config.yml"
+append :linked_files, "config/database.yml", "config/app_config.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
