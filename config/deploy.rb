df599abc1d2fdc60fc57550aa0f8be44430cd56e
Stuart Marquis <stuartmarquis@gmail.com>
2019-06-14 12:29:08 -0400

    cap deploy set :passenger_restart_with_touch, true

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Deploy account doesn't directly call passenger
+set :passenger_restart_with_touch, true
+
