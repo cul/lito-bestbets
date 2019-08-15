2a95663bfc0948fc442428fae6cbf948faca97f0
Eric O <eric@ejdigitalmedia.com>
2019-08-15 16:37:24 -0400

    Style updates

--- app/javascript/packs/key_resources.js
+++ app/javascript/packs/key_resources.js
@@ -2,6 +2,7 @@
 import '../key_resources/stylesheets/key_resources.scss';
 
 import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   //console.log('ready!');
