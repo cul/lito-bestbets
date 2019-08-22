c8a4248efa8068a2ac71f5e2cab25df4fb7157ed
Eric O <eric@ejdigitalmedia.com>
2019-08-22 14:17:42 -0400

    FontAwesome node module should have been in dependencies rather than devDependencies. Now it is!

--- app/javascript/packs/key_resources.js
+++ app/javascript/packs/key_resources.js
@@ -2,6 +2,8 @@
 import '../key_resources/stylesheets/key_resources.scss';
 
 import 'bootstrap'
+
+// Add app-wide support for FontAwesome
 import '@fortawesome/fontawesome-free/js/all.js'
 
 document.addEventListener('DOMContentLoaded', () => {
